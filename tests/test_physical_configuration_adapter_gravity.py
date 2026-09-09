a78b07b4306d43af0e9ca17c083908a947e95de2
natajdaniyal <natajdaniyal@gmail.com>
2026-09-09 20:12:31 +0330

    Add regression test for default gravity

---
+++ tests/test_physical_configuration_adapter_gravity.py
@@ -0,0 +1,21 @@
+from physical_configuration import (
+    BodyPhysicalConfiguration,
+    StagePhysicalConfiguration,
+)
+from simulation.physical_configuration_adapter import simulation_from_configuration
+
+
+def test_default_configuration_simulation_uses_gravity():
+    configuration = StagePhysicalConfiguration()
+    configuration.add_body(
+        BodyPhysicalConfiguration(name="A", mass=1.0, position_x=0.0)
+    )
+    configuration.add_body(
+        BodyPhysicalConfiguration(name="B", mass=1.0, position_x=1.0)
+    )
+
+    simulation = simulation_from_configuration(configuration, time_step=0.1)
+    simulation.step()
+
+    assert simulation.bodies[0].velocity.x > 0
+    assert simulation.bodies[1].velocity.x < 0
