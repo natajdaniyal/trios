e76fa157f416ff981519e1c96baf153fd973e2ba
natajdaniyal <natajdaniyal@gmail.com>
2026-09-16 13:26:45 +0330

    Fix parameter rules test import path

--- tests/test_parameter_rules.py
+++ tests/test_parameter_rules.py
@@ -1,3 +1,14 @@
+import os
+import sys
+
+sys.path.append(
+    os.path.dirname(
+        os.path.dirname(
+            os.path.abspath(__file__)
+        )
+    )
+)
+
 import pytest
 
 from parameter_rules import ParameterRule, SelectionRules
